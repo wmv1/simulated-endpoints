f00f4ada2e94cd4544cf6f3dc1040a5a1f946e8c
Willian Vieira <willianmarquesv@gmail.com>
2020-10-16 19:41:57 -0300

    Refactoring ssl verification

--- lib/generic_client.rb
+++ lib/generic_client.rb
@@ -1,34 +1,47 @@
 require "net/https"
 require "uri"
-require 'openssl'
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+VERIFY_SSL = ApiStub.config.ssl_valitade
 
 class GenericClient
   def get(path = '')
-    Faraday.get(url+path, {ticket: service_ticket},
+    http_connection.get(url+path, {ticket: service_ticket},
       "Service-Ticket" => service_ticket)
   end
 
   def post(path = '', params = {})
-    Faraday.put(url+path, params.as_json.to_json,
+    http_connection.post(url+path, filtered_params(params),
       "Content-Type" => "application/json",
       "Service-Ticket" => service_ticket)
   end
 
   def put(path = '', params = {})
-    Faraday.put(url+path, params.as_json.to_json,
+    http_connection.put(url+path, filtered_params(params),
       "Content-Type" => "application/json",
       "Service-Ticket" => service_ticket)
   end
 
   private
 
+  def filtered_params(params)
+    params.as_json["home"].to_json
+  end
+
+  def http_connection
+    Faraday.new(ssl: {verify: VERIFY_SSL})
+  end
+
   def url
     ApiStub.config.origin_host
   end
 
   def service_ticket
+    return '' unless system_cas_configured?
+
     Cassette::Client.st_for(ApiStub.config.origin_service_name)
+  end
+
+  def system_cas_configured?
+    ApiStub.config.systems_cas.base.present? && ApiStub.config.systems_cas.base != 'https://$cas-base/'
   end
 end
